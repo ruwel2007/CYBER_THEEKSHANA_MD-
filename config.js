65ca61c848f60fbd5161e7c6bf0900bc4a90978a
ruwel2007 <138858782+ruwel2007@users.noreply.github.com>
2024-06-21 11:03:51 +0530

    KING AMW MD

--- config.js
+++ config.js
@@ -3,15 +3,15 @@ const chalk = require('chalk');
 
 
 // Other
-global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
-global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
+global.link = 'https://whatsapp.com/channel/0029VaRvwpo9WtC71ZpbBx0K'
+global.linkGroup = 'https://chat.whatsapp.com/E0b13EB30fCKhurY8hL8Pc'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.ownername = "CYBER THEEKSHANA";
+global.owner = ['94753335072'];
+global.premium = ['94753335072'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.author = "KING AMW MD";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
@@ -19,13 +19,13 @@ global.menuType = "2"
 global.autoswview = true
 global.autoread = false
 global.available = false
-global.autoTyping = false
-global.autoRecord = false
-global.autoreact = false
+global.autoTyping = true 
+global.autoRecord = true 
+global.autoreact = true
 global.onlygroup = false
 global.onlypc = false
 global.welcome = true
-global.autoBlock = true
+global.autoBlock = false 
  
 /*REPLY MESSAGE*/
 global.mess = {
@@ -34,7 +34,7 @@ global.mess = {
     success: '`✓ Success`',
     admin: '`This feature is only for group admins`',
     botAdmin: '`I am not an admin!`',
-    owner: '`You are not my owner`',
+    owner: '`*උබද කැරියා මෙක හැදුවේ ඇ*',
     group: '`You can use this command only in groups ❌`',
     private: '`Feature is used only for private chats!`',
     bot: '`Bot number user special features`',
